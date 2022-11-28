558789693198d1c990af4e6146daff79c97ec1c2
Ethan O'Brien <ethan.a.obrien@gmail.com>
2022-11-27 19:05:02 -0600

    listen on local network

--- index.js
+++ index.js
@@ -316,7 +316,7 @@ function makeServer(port, startIO) {
     }
 
 
-    server.listen(port || 3000, () => {
+    server.listen(port || 3000, '0.0.0.0', () => {
         console.log('The Main Server is now running on port :' + (port || 3000));
     });
     killable(server);
